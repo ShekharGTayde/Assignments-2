e16a31dd75a971f3ee18b95b3e8286850d6e2ee7
Shekhar Tayde <shekhartayde410@gmail.com>
2024-07-28 12:16:58 +0530

    first change

--- Assignments-2/Git Assignment/script.js
+++ Assignments-2/Git Assignment/script.js
@@ -1,31 +1,69 @@
-/**
- * Create a file named script.js with a simple JavaScript function.
- */
+// /**
+//  * Create a file named script.js with a simple JavaScript function.
+//  */
+// /** Task-3:
+// Add a new function in script.js that displays a personalized greeting.
+//  */
 
-function greeting(name){
-    console.log("Hello ${name},best wishes for your development learning")
-}
-greeting("shekhar")
+// function greeting(name){
+//     console.log("Hello ${name},best wishes for your development learning")
+// }
+// greeting("shekhar")
 
-/**Task-1:
-Initialize a Git repository for your JavaScript project. */
+// /**Task-1:
+// Initialize a Git repository for your JavaScript project. */
+// git init
 
-/**
- * Task-3:
-Add and commit the changes.
- */
 
-/**
- * 2
-Task-1:
-Create a new branch named feature-greeting.
- */
 
-/**
- * Task-2:
-Switch to the new branch.
- */
-/**
- * Task-3:
-Add a new function in script.js that displays a personalized greeting.
- */
+// /**
+//  * Task-3:
+// Add and commit the changes.
+//  */
+// git add .
+// git commit -m "first changes"
+// /**
+//  * 2
+// Task-1:
+// Create a new branch named feature-greeting.
+//  */
+// git branch -M feature-greeting 
+// /**
+//  * Task-2:
+// Switch to the new branch.
+//  */
+// git branch -M main
+// /**
+
+
+// /**
+//  * Task-4:
+// Commit the changes in the feature branch.
+//  */
+// git commit -m "second  changes"   
+
+// /**
+//  * Task-6:
+// Switch back to the main branch.
+//  */
+// git switch main
+
+// /**
+//  * ®3
+// Task-1;
+// ¤ Create a GitHub repository for your JavaScript project.
+//  */
+
+// /**
+//  * Task-2§
+// ¤ Push your local repository to the GitHub repository.
+//  */
+// git push -u origin main
+
+// /**
+//  * Task-5§
+// ¤ Fetch and merge the changes made by your team member.
+//  */
+// git fetch
+// git checkout main
+// git merge origin/main
