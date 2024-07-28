0bfb0ef6a125bb921e53a0f11a1bebd408ac828a
Shekhar Tayde <shekhartayde410@gmail.com>
2024-07-28 11:35:26 +0530

    second  changes

--- Assignments-2/Git Assignment/script.js
+++ Assignments-2/Git Assignment/script.js
@@ -1,4 +1,31 @@
-function greeting(){
-    console.log("Hello,best wishes for your development learning")
+/**
+ * Create a file named script.js with a simple JavaScript function.
+ */
+
+function greeting(name){
+    console.log("Hello ${name},best wishes for your development learning")
 }
-greeting();
+greeting("shekhar")
+
+/**Task-1:
+Initialize a Git repository for your JavaScript project. */
+
+/**
+ * Task-3:
+Add and commit the changes.
+ */
+
+/**
+ * 2
+Task-1:
+Create a new branch named feature-greeting.
+ */
+
+/**
+ * Task-2:
+Switch to the new branch.
+ */
+/**
+ * Task-3:
+Add a new function in script.js that displays a personalized greeting.
+ */
